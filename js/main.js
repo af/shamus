dc19846825ddb679736ff745e5ac3412a5a26587
Aaron Franks <aaron.franks@gmail.com>
2013-08-25 08:24:32 -0700

    Move js to a separate file, and add some rudimentary css.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// To build & run:
+// zip -r app.nw index.html css js package.json; ./node-webkit.app/Contents/MacOS/node-webkit app.nw
+
+
+// Setup our window object.
+// See https://github.com/rogerwang/node-webkit/wiki/Window
+var gui = require('nw.gui');
+var win = gui.Window.get();
+win.title = 'Checkpoint';
+
+var bus = new require('events').EventEmitter;
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+// TODO: make these user-specifiable:
+var watchRegex = new RegExp('\.js$');
+var userCmd = 'mocha test.js -R tap';
+
+fs.watch('.', function(evt, filename) {
+    if (watchRegex.test(filename)) {
+        console.log(filename + ' changed!');
+        exec(userCmd, function(err, stdout, stderr) {
+            if (err) return console.error('ERROR WHEN RUNNING TESTS');
+            console.log('STDOUT:', stdout);
+        });
+    }
+});
+
