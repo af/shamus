0dce5eeb62d8ea0c734533f214fca462716e8398
Aaron Franks <aaron.franks@gmail.com>
2013-08-31 13:20:45 -0700

    Render the view from an underscore template.

--- js/taskview.js
+++ js/taskview.js
@@ -1,24 +1,29 @@
 var Backbone = require('backbone');
+var _ = require('underscore');
 
 module.exports = Backbone.View.extend({
     tagName: 'li',
+    template: _.template(
+        '<div class="status"> <div></div> </div> <%= name %>'
+    ),
 
     initialize: function() {
         this.render();
         this.$status = this.$('.status');
-        this.listenTo(this.model, 'change:isRunning', function(task) {
-            console.log('in view handler');
-            if (task.isRunning) this.$status.removeClass('ok error').addClass('running');
-            else {
-                if (task.isOK) this.$status.removeClass('running error').addClass('ok');
-                else this.$status.removeClass('running ok').addClass('error');
-            }
-        });
+
+        this.listenTo(this.model, 'change:isRunning', this.updateStatus.bind(this));
+    },
+
+    updateStatus: function(task) {
+        if (task.isRunning) this.$status.removeClass('ok error').addClass('running');
+        else {
+            if (task.isOK) this.$status.removeClass('running error').addClass('ok');
+            else this.$status.removeClass('running ok').addClass('error');
+        }
     },
 
     render: function() {
-        // TODO: use template
-        this.$el.html('<div class="status"> <div></div> </div>');
+        this.$el.html(this.template(this.model));
         return this;
     }
 });
