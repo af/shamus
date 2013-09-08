5af4886771a1e56a0d7d7f8233c6a6f651a4714d
Aaron Franks <aaron.franks@gmail.com>
2013-09-08 15:55:26 -0700

    First pass at using a .sentinel.json file to load tasks.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 
 // Setup our window object.
 // See https://github.com/rogerwang/node-webkit/wiki/Window
+var fs = require('fs');
 var gui = require('nw.gui');
 var win = gui.Window.get();
 win.title = 'Sentinel';
@@ -11,37 +12,16 @@ win.title = 'Sentinel';
 var Task = require('./js/task');
 var TaskView = require('./js/taskview');
 
-var t = new Task({
-    name: 'Sentinel mocha runner',
-    command: 'mocha tests -R tap',
-    parser: 'tap',
-    watchMatcher: '\\.js$'
-});
-
-var t2 = new Task({
-    name: 'Sentinel stylus',
-    command: 'stylus css',
-    parser: 'exitcode',
-    watchMatcher: '\\.styl$'
-});
-
-var t3 = new Task({
-    name: 'Sentinel JSHint',
-    command: 'jshint js',
-    parser: 'exitcode',
-    watchMatcher: '\\.js$'
-});
-
-Task.startLoop();
-
-
 var Backbone = require('backbone');
 var $ = require('littledom');
 Backbone.$ = $;
 
-var v = new TaskView({ model: t });
-var v2 = new TaskView({ model: t2 });
-var v3 = new TaskView({ model: t3 });
-$('#taskContainer')[0].appendChild(v.el);
-$('#taskContainer')[0].appendChild(v2.el);
-$('#taskContainer')[0].appendChild(v3.el);
+var taskFile = process.argv[2] || '.sentinel.json';
+var taskList = JSON.parse(fs.readFileSync(taskFile, 'utf8'));
+taskList.tasks.forEach(function(taskSpec) {
+    var t = new Task(taskSpec);
+    var v = new TaskView({ model: t });
+    $('#taskContainer')[0].appendChild(v.el);
+});
+
+Task.startLoop();
