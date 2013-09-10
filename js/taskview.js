2f41c3d2d2c54f93c2659190243f0e1f5b721986
Aaron Franks <aaron.franks@gmail.com>
2013-09-10 08:01:22 -0700

    First pass at styling error messages.

--- js/taskview.js
+++ js/taskview.js
@@ -4,13 +4,13 @@ var _ = require('underscore');
 module.exports = Backbone.View.extend({
     tagName: 'li',
     template: _.template(
-        '<div class="status"> <div></div> </div> <div><h1><%= name %></h1></div> <div class="error"></div>'
+        '<div class="status"> <div></div> </div> <div><h1><%= name %></h1></div> <div class="errorMsg"></div>'
     ),
 
     initialize: function() {
         this.render();
         this.$status = this.$('.status');
-        this.$error = this.$('.error');
+        this.$error = this.$('.errorMsg');
 
         this.listenTo(this.model, 'change:isRunning', this.updateStatus.bind(this));
         this.listenTo(this.model, 'error', this.showError.bind(this));
