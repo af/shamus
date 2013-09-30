bb09e0ae97a7635f4195840af9fee3ed619d8a34
Aaron Franks <aaron.franks@gmail.com>
2013-09-30 08:01:01 -0700

    Move app-wide UI logic to an AppView class.

---
+++ js/appview.js
@@ -0,0 +1,35 @@
+var Backbone = require('backbone');
+
+// AppView
+// A Backbone view to manage UI at the app/window level.
+// document.body should be passed in as view.el.
+module.exports = Backbone.View.extend({
+    menubarHeight: 22,     // Accurate on OS X 10.8, probably needs to be adjusted for platform
+
+    initWindow: function(window) {
+        this.window = window;
+        this.screen = window.screen;
+
+        this.moveWindow();
+        this.resizeWindow();
+    },
+
+    // Position the window in the top right of the screen on startup.
+    // TODO: make window position a config option and pass it in
+    moveWindow: function() {
+        this.window.moveTo(this.screen.width - this.window.outerWidth, 0);
+    },
+
+    // Resize window so it takes exactly the same height as the list of tasks:
+    resizeWindow: function() {
+        var view = this;
+        var w = this.window;
+
+        // Give webkit a little time to re-render the resized elements before we detect the
+        // height to change the window to:
+        setTimeout(function() {
+            var height = w.getComputedStyle(view.el).height;
+            w.resizeTo(w.outerWidth, parseFloat(height) + view.menubarHeight);
+        }, 10);
+    }
+});
--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ var Backbone = require('backbone');
 var $ = require('littledom');
 Backbone.$ = $;
 
+var AppView = require('./js/appview');
+var app = new AppView({ el: document.body });
+
 var taskFile = process.argv[2] || '.sentinel.json';
 var taskList = JSON.parse(fs.readFileSync(taskFile, 'utf8'));
 taskList.tasks.forEach(function(taskSpec) {
@@ -23,25 +26,8 @@ taskList.tasks.forEach(function(taskSpec) {
     var v = new TaskView({ model: t });
     $('#taskContainer')[0].appendChild(v.el);
 
-    v.on('changeStatus', resizeWindow);
+    v.on('changeStatus', app.resizeWindow.bind(app));
 });
 
+app.initWindow(window);
 Task.startLoop();
-resizeWindow();
-
-
-// Resize window so it takes exactly the same height as the list of tasks:
-function resizeWindow() {
-    var menubarHeight = 22;     // Accurate on OS X 10.8, probably needs to be adjusted for platform
-
-    // Give webkit a little time to re-render the resized elements before we detect the new
-    // height to change the window to:
-    setTimeout(function() {
-        var height = getComputedStyle(document.body).height;
-        window.resizeTo(window.outerWidth, parseFloat(height) + menubarHeight);
-    }, 10);
-}
-
-// Position the window in the top right of the screen on startup.
-// TODO: make this a config option
-window.moveTo(screen.width - window.outerWidth, 0);
