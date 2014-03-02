dd45e9f865bd5a50636df6ff09c9f131dcc94d8a
Aaron Franks <aaron.franks@gmail.com>
2014-03-01 21:45:53 -0800

    Convert tests to use tape.

--- tests/test_task.js
+++ tests/test_task.js
@@ -1,14 +1,14 @@
-var assert = require('assert');
+var test = require('tape');
 var Task = require('../js/task');
 
-describe('Task model', function() {
-    it('parses a string command to spawn()\'s format', function() {
-        var t = new Task();
-        var result = t.parseCommand('foo 1 2 3');
-        assert.strictEqual(result.cmd, 'foo');
-        assert.strictEqual(result.args.length, 3);
-        assert.strictEqual(result.args[0], '1');
-        assert.strictEqual(result.args[1], '2');
-        assert.strictEqual(result.args[2], '3');
-    });
+test('Task model command parsing', function(t) {
+    t.plan(5);
+
+    var task = new Task();
+    var result = task.parseCommand('foo 1 2 3');
+    t.equal(result.cmd, 'foo');
+    t.equal(result.args.length, 3);
+    t.equal(result.args[0], '1');
+    t.equal(result.args[1], '2');
+    t.equal(result.args[2], '3');
 });
